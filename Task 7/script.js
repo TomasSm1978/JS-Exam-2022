170daf35a3130f5208884e548246611c11ee1cb3
TomasSm1978 <99352018+TomasSm1978@users.noreply.github.com>
2022-05-01 22:25:00 +0300

    Pirminis sprendimas

---
+++ Task 7/script.js
@@ -0,0 +1,19 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+function showObjectKeys(x) {
+  const JS7_1 = Object.keys(x);
+  console.log(JS7_1);
+  return JS7_1;
+}
+showObjectKeys(audi);
